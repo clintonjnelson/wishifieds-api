e16bb263b0b69e9500ce42da2d73cc5187aa66c5
clintonjnelson <clintonjnelson@live.com>
2017-03-30 21:53:15 -0700

    add search filtering for status and results Limit; refactor to shared query method with sort and skip for future pagination

--- routes/search_routes.js
+++ routes/search_routes.js
@@ -8,32 +8,59 @@ var Sign       = require('../models/Sign.js');
 module.exports = function(router) {
   router.use(bodyparser.json());
 
+
+  // Search
   router.get('/search', function(req, res) {
     console.log("SEARCH STRING RECEIVED AS: ", req.query.searchStr);
 
     var searchStr         = req.query.searchStr.trim();
     var partialMatchRegex = new RegExp('^.*' + searchStr + '.*$', 'i');  // ^.*George.*$
+    var regexQuery        = {'$regex': partialMatchRegex}
     var currentCount      = 0;    // Count tracking
-    var totalSearchTypes  = 2;    // Total types of DB responses we'll expect
     var searchResults     = {};   // Object to populate for response
+    var resultsLimit      = 100;
+
+    // MAY HAVE TO GO BACK TO THIS AS SEARCHES GET MORE COMPLEX
+    // var totalSearchTypes  = 2;    // Total DB queries before response
+    // User.find({'username': {'$regex': partialMatchRegex} }, buildQueryCallback('users'));
+    // Sign.find({'knownAs':  {'$regex': partialMatchRegex} }, buildQueryCallback('signs'));
+
+    var searches = [
+      { Model: User, query: { username: regexQuery }, queryField: 'username', resultsType: 'users' },
+      { Model: Sign, query: { knownAs:  regexQuery }, queryField: 'knownAs' , resultsType: 'signs' }
+    ];
+
+    searches.forEach(function(search) {
+      queryCombineAndRespondWithResults(search.Model, search.query, search.queryField, search.resultsType);
+    });
 
 
-    User.find({'username': {'$regex': partialMatchRegex} }, buildQueryCallback('users'));
-    Sign.find({'knownAs':  {'$regex': partialMatchRegex} }, buildQueryCallback('signs'));
+    // ---------- Helpers ----------
+    function queryCombineAndRespondWithResults(Model, query, queryField, resultsType) {
+      Model
+        .find(query)
+        .sort(queryField)                            // alphabetical by name
+        .skip(0)                                // USE LATER FOR PAGINATION (pag-number * resultsLimit)
+        .limit(resultsLimit)                    // Limit results returned (DYNAMIC LATER?)
+        .exec(buildQueryCallback(resultsType)); //execute with callback
+    }
 
+    // Puts results into the results array and responds once last search is done
     function buildQueryCallback(type) {
       return function queryCallback(err, results) {
         if(err) {return res.status(500).json({error: true, msg: 'database error'});}
         console.log(type + " FOUND AS: ", results);
 
-        searchResults[type] = results;  // searchResults: {users: results}
-        currentCount++;                 // Track types iterated through
-        responseCheck();                // Return yet? Only if added all types.
+        // Return only Active (LATER MAYBE PROVIDE OTHER OPTIONS BASED ON REQUEST);
+        var filteredResults = results.filter( function(result) { return result.status === 'A' } );
+        searchResults[type] = filteredResults;  // searchResults: {users: results}
+        currentCount++;                         // Track types iterated through
+        responseCheck();                        // Return yet? Only if added all types.
       };
 
       // ensure all searches run; send when done
       function responseCheck() {
-        if(currentCount === totalSearchTypes) {
+        if(currentCount === searches.length) {
           return res.json(searchResults);
         }
       }
