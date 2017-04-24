5d01c5b0392b28828d6d98e4d90aebb7ed228dd7
clintonjnelson <clintonjnelson@live.com>
2017-04-24 11:56:05 -0700

    configure for heroku, make server start dependent on db connect, clean up old angular1 & webpack deps from server

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,34 +3,14 @@
 module.exports = function(grunt) {
 
   // Load Tasks
-  grunt.loadNpmTasks('grunt-contrib-clean' );  // webpack: clean folder
-  grunt.loadNpmTasks('grunt-contrib-copy'  );  // webpack: copy assets
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-jscs'          );
   grunt.loadNpmTasks('grunt-mocha-test'    );
   grunt.loadNpmTasks('grunt-nodemon'       );  // avoid server restarts
   grunt.loadNpmTasks('grunt-contrib-watch' );
-  grunt.loadNpmTasks('grunt-webpack'       );
 
   // Configure Tasks
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
-    clean: {
-      dev: {
-        src: 'build/' // clean out webpack build folder
-      }
-    },
-    copy: {
-      htmlcss: {
-        cwd: 'app/',
-        expand: true,
-        flatten: false,
-        src: ['**/*.html', '**/*.css', '**/*.ico'],
-        dest: 'build/',
-        filter: 'isFile'
-      }
-    },
     jscs: {
       src: ['Gruntfile.js',
             '*.js',
@@ -77,40 +57,9 @@ module.exports = function(grunt) {
     watch: {
 
     },
-    webpack: {
-      client: { // webpack frontend production
-        entry: __dirname + '/app/js/client.js',
-        output: {
-          path: 'build/',
-          file: 'bundle.js'
-        },
-        stats: {
-          colors: true
-        },
-        failOnError: false,
-        watch: true,
-        keepalive: true
-      },
-      test: { // webpack frontend tests
-        entry: __dirname + '/test/client/test.js',
-        output: {
-          path: 'test/client/',
-          file: 'test_bundle.js'
-        },
-        stats: {
-          colors: true
-        },
-        failOnError: false,
-        watch: false,
-        keepalive: true
-      }
-    }
   });
 
   // Custom Task Chains
   grunt.registerTask('test',       ['jshint:dev', 'jscs', 'mochaTest'   ]);
-  grunt.registerTask('build:dev',  ['copy:htmlcss', 'webpack:client'    ]);
-  grunt.registerTask('build:test', ['copy:htmlcss', 'webpack:test'      ]);
-  grunt.registerTask('build',      ['build:dev'                         ]);
-  grunt.registerTask('default',    ['test', 'build'                     ]);
+  grunt.registerTask('default',    ['test']);
 };
