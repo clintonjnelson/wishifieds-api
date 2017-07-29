83d91d1da5c8d8a0ae9c6485ac38cec07852e557
clintonjnelson <clintonjnelson@live.com>
2017-07-28 19:10:21 -0700

    add userCleanup task route - rough implementation

--- routes/tasks_routes.js
+++ routes/tasks_routes.js
@@ -5,7 +5,7 @@ var bodyparser = require('body-parser');
 var eatAuth    = require('../lib/routes_middleware/eat_auth.js'  )(process.env.AUTH_SECRET);
 var eatOnReq   = require('../lib/routes_middleware/eat_on_req.js');
 var updateSitemap = require('../lib/tasks/updateSitemap.js');
-
+var userCleanup   = require('../lib/tasks/userCleanup.js');
 
 module.exports = function(router) {
   router.use(bodyparser.json());
@@ -23,6 +23,19 @@ module.exports = function(router) {
     } else {
       return res.status(500).json({success: false, error: true});
     }
+  });
+
+  router.get('/tasks/cleanupusers', function(req, res) {
+    console.log("MADE IT TO THE ROUTE...");
+
+    userCleanup(function(err) {
+      if(err) {
+        console.log("Error cleaning up users.");
+        return res.status(500).json({success: false, error: true});
+      }
+
+      return res.status(200).json({success: true, error: false});
+    });
   });
 }
 
