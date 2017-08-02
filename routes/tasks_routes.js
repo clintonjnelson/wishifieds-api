ee5bc48a0b5e15e3743aa805a90cba624ba84769
clintonjnelson <clintonjnelson@live.com>
2017-08-01 18:59:26 -0700

    add admin auth and PUT route with trigger onto route

--- routes/tasks_routes.js
+++ routes/tasks_routes.js
@@ -13,29 +13,35 @@ module.exports = function(router) {
   router.put('/tasks/sitemap', eatOnReq, eatAuth, adminAuth, function(req, res) {
 
     if(req.body.trigger === true) {
-
       // Update Sitemap
       updateSitemap(req, function(error) {
         if(error) { return res.status(500).json({success: false, error: true}); }
 
         return res.status(200).json({success: true, error: false});
       });
-    } else {
+    }
+    else {
       return res.status(500).json({success: false, error: true});
     }
   });
 
-  router.get('/tasks/cleanupusers', function(req, res) {
+  router.put('/tasks/cleanupusers', eatOnReq, eatAuth, adminAuth, function(req, res) {
     console.log("MADE IT TO THE ROUTE...");
 
-    userCleanup(function(err) {
-      if(err) {
-        console.log("Error cleaning up users.");
-        return res.status(500).json({success: false, error: true});
-      }
+    if(req.body.trigger === true) {
+      // Cleanup Old Unused Users
+      userCleanup(function(err) {
+        if(err) {
+          console.log("Error cleaning up users.");
+          return res.status(500).json({success: false, error: true});
+        }
 
-      return res.status(200).json({success: true, error: false});
-    });
+        return res.status(200).json({success: true, error: false});
+      });
+    }
+    else {
+      return res.status(500).json({success: false, error: true});
+    }
   });
 }
 
