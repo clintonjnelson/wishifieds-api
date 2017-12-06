8889417ccfb56d17d357978104a9b799bf7a9ed5
clintonjnelson <clintonjnelson@live.com>
2017-12-05 21:24:16 -0800

    fix the ensureIndex issue - failing because duplicate records in there already

--- models/Interaction.js
+++ models/Interaction.js
@@ -19,7 +19,17 @@ var InteractionSchema = mongoose.Schema({
 
 // Instead of writing a validator (inefficient), just ensure uniqueness on 2-fields
 // This will ensure there is only one guid per date logged
-InteractionSchema.index({guid: 1, eventDate: 1}, {unique: true});
+InteractionSchema.index({guid: 1, eventDate: 1, targetCategory: 1, targetType: 1}, {unique: true});
 
 // export schema
 module.exports = mongoose.model('Interaction', InteractionSchema);
+
+
+// Checking for errors in insuring index (found!):
+// var Interactions = module.exports = mongoose.model('Interaction', InteractionSchema);
+// Interactions.ensureIndexes(function(err) {
+//   console.log('ENSURE INDEX');
+//   if(err) { console.log(err); }
+// });
+// Alternate method is to listen to the indexing command for any errors:
+// Model.on('index', function(error) { /* log error */ });
