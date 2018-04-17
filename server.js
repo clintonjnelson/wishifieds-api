de6b3d94dea93c7559cb5d9beb1ac9ce493e3423
clintonjnelson <clintonjnelson@live.com>
2018-04-16 21:55:02 -0700

    updated the server.js to working

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ var express     = require('express'        );
 var compression = require('compression'    );
 var fs          = require('fs'             );
 var https       = require('https'          );
-var mongoose    = require('mongoose'       );
+// var mongoose    = require('mongoose'       );
 var passport    = require('passport'       );
-var session     = require('express-session');  // Oauth1 provider workaround
+// var session     = require('express-session');  // Oauth1 provider workaround
 var ensureHttps = require('./lib/routes_middleware/ensure_https.js');
 var app         = express();
 
 
 // Routers
 var authRouter         = new express.Router();
-var oauthRouter        = new express.Router();
-var interactionsRouter = new express.Router();
-var dashboardsRouter   = new express.Router();
-var searchRouter       = new express.Router();
-var signsRouter        = new express.Router();
-var tasksRouter        = new express.Router();
-var usersRouter        = new express.Router();
+// var oauthRouter        = new express.Router();
+// var interactionsRouter = new express.Router();
+// var dashboardsRouter   = new express.Router();
+// var searchRouter       = new express.Router();
+// var signsRouter        = new express.Router();
+// var tasksRouter        = new express.Router();
+// var usersRouter        = new express.Router();
 
 // Redirect any http to https
 app.use(ensureHttps);
@@ -28,55 +28,55 @@ app.use(ensureHttps);
 app.use(compression());
 
 // Initialize passport middleware & configure with passport_strategy.js
-app.use(session({secret: 'oauth1sucks', id: 'oauth', maxAge: null}));
+// app.use(session({secret: 'oauth1sucks', id: 'oauth', maxAge: null}));
 app.use(passport.initialize());
 app.use(passport.session());            // only for oauth1 to work
 
 // Load passport with strategies
 require('./lib/passport_strategies/basic.js'        )(passport);
-require('./lib/passport_strategies/amazon.js'       )(passport);
-require('./lib/passport_strategies/deviantart.js'   )(passport);
-require('./lib/passport_strategies/disqus.js'       )(passport);
-require('./lib/passport_strategies/etsy.js'         )(passport);
-require('./lib/passport_strategies/facebook.js'     )(passport);
-require('./lib/passport_strategies/foursquare.js'   )(passport);
-require('./lib/passport_strategies/github.js'       )(passport);
-require('./lib/passport_strategies/google.js'       )(passport);
-require('./lib/passport_strategies/imgur.js'        )(passport);
-require('./lib/passport_strategies/instagram.js'    )(passport);
-require('./lib/passport_strategies/linkedin.js'     )(passport);
-require('./lib/passport_strategies/patreon.js'      )(passport);
-require('./lib/passport_strategies/pinterest.js'    )(passport);
-require('./lib/passport_strategies/reddit.js'       )(passport);
-require('./lib/passport_strategies/spotify.js'      )(passport);
-require('./lib/passport_strategies/stackexchange.js')(passport);
-require('./lib/passport_strategies/tumblr.js'       )(passport);
-require('./lib/passport_strategies/twitter.js'      )(passport);
-require('./lib/passport_strategies/vimeo.js'        )(passport);
-require('./lib/passport_strategies/vk.js'           )(passport);
-require('./lib/passport_strategies/wordpress.js'    )(passport);
-require('./lib/passport_strategies/youtube.js'      )(passport);
+// require('./lib/passport_strategies/amazon.js'       )(passport);
+// require('./lib/passport_strategies/deviantart.js'   )(passport);
+// require('./lib/passport_strategies/disqus.js'       )(passport);
+// require('./lib/passport_strategies/etsy.js'         )(passport);
+// require('./lib/passport_strategies/facebook.js'     )(passport);
+// require('./lib/passport_strategies/foursquare.js'   )(passport);
+// require('./lib/passport_strategies/github.js'       )(passport);
+// require('./lib/passport_strategies/google.js'       )(passport);
+// require('./lib/passport_strategies/imgur.js'        )(passport);
+// require('./lib/passport_strategies/instagram.js'    )(passport);
+// require('./lib/passport_strategies/linkedin.js'     )(passport);
+// require('./lib/passport_strategies/patreon.js'      )(passport);
+// require('./lib/passport_strategies/pinterest.js'    )(passport);
+// require('./lib/passport_strategies/reddit.js'       )(passport);
+// require('./lib/passport_strategies/spotify.js'      )(passport);
+// require('./lib/passport_strategies/stackexchange.js')(passport);
+// require('./lib/passport_strategies/tumblr.js'       )(passport);
+// require('./lib/passport_strategies/twitter.js'      )(passport);
+// require('./lib/passport_strategies/vimeo.js'        )(passport);
+// require('./lib/passport_strategies/vk.js'           )(passport);
+// require('./lib/passport_strategies/wordpress.js'    )(passport);
+// require('./lib/passport_strategies/youtube.js'      )(passport);
 
 // Populate Routes
-require('./routes/oauth_routes.js')(oauthRouter, passport);
+// require('./routes/oauth_routes.js')(oauthRouter, passport);
 require('./routes/auth_routes.js' )(authRouter,  passport);
-require('./routes/interactions_routes.js')(interactionsRouter);
-require('./routes/dashboards_routes.js')(dashboardsRouter);
-require('./routes/search_routes.js')(searchRouter);
-require('./routes/signs_routes.js' )(signsRouter );
-require('./routes/users_routes.js' )(usersRouter );
-require('./routes/tasks_routes.js')(tasksRouter  );
+// require('./routes/interactions_routes.js')(interactionsRouter);
+// require('./routes/dashboards_routes.js')(dashboardsRouter);
+// require('./routes/search_routes.js')(searchRouter);
+// require('./routes/signs_routes.js' )(signsRouter );
+// require('./routes/users_routes.js' )(usersRouter );
+// require('./routes/tasks_routes.js')(tasksRouter  );
 
 
 // Route middleware
 app.use('/api', authRouter  );
-app.use('/api', oauthRouter );
-app.use('/api', searchRouter);
-app.use('/api', signsRouter );
-app.use('/api', usersRouter );
-app.use('/api', interactionsRouter);
-app.use('/api', dashboardsRouter);
-app.use('/api', tasksRouter );
+// app.use('/api', oauthRouter );
+// app.use('/api', searchRouter);
+// app.use('/api', signsRouter );
+// app.use('/api', usersRouter );
+// app.use('/api', interactionsRouter);
+// app.use('/api', dashboardsRouter);
+// app.use('/api', tasksRouter );
 
 // Static Resources
 var dir = process.env.WEBPACK_DIRECTORY || './client/dist';
@@ -102,12 +102,12 @@ if(process.env.NODE_ENV === 'dev') {
 
 
 // Set mongoose connection - server is conditional on that connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/signpost', function(error) {
-  if(error) {
-    console.log('Error connecting to Mongo DB. Error is: ', error);
-    return new Error('Connection to Mongo DB failed. Stopping server from starting.');
-  }
-  console.log('Starting http & https servers...');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/signpost', function(error) {
+//   if(error) {
+//     console.log('Error connecting to Mongo DB. Error is: ', error);
+//     return new Error('Connection to Mongo DB failed. Stopping server from starting.');
+//   }
+//   console.log('Starting http & https servers...');
 
   // DB connection success => start server
   app.listen(process.env.PORT || process.env.HTTP_PORT || 3000, function() {
@@ -119,5 +119,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/signpost', func
       console.log("https server running on port " + (process.env.HTTPS_PORT ||  8443));
     });
   }
-});
+// });
 
